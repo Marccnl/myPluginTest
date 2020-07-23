92c96f525436825b58cc3c55eec4577594bc4dcb
1kweethetookNiet! <marc.nl@outlook.com>
2020-07-23 16:13:58 +0200

    dingen

--- www/arQiveCamera2.js
+++ www/arQiveCamera2.js
@@ -1,13 +1,34 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+var pluginName = 'MyCam';
+
+function MyCam() {}
+
+// MyCam.prototype.transcodeVideo = function(success, error, options) {
+//   var self = this;
+//   var win = function(result) {
+//     if (typeof result.progress !== 'undefined') {
+//       if (typeof options.progress === 'function') {
+//         options.progress(result.progress);
+//       }
+//     } else {
+//       success(result);
+//     }
+//   };
+//   exec(win, error, pluginName, 'transcodeVideo', [options]);
+// };
+
+MyCam.prototype.coolMethod = function (arg0, success, error) {
     exec(success, error, 'arQiveCamera2', 'coolMethod', [arg0]);
 };
 
-module.exports.add = function(arg0, success, error) {
+MyCam.prototype.add = function(arg0, success, error) {
     exec(success, error, 'coolMethod', 'add', [arg0]);
 }
 
-module.exports.subtract = function(arg0, success, error) {
-    exec(success, error, 'coolMethod', 'add', [arg0]);
+MyCam.prototype.subtract = function(arg0, success, error) {
+    exec(success, error, 'coolMethod', 'subtract', [arg0]);
 }
+
+
+module.exports = new MyCam();
